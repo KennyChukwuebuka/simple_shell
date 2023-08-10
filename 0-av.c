794d9846c63d0b6617162e497ae3fbc998111b37
kennyChukwuebuka <kenkinado@gmail>
2023-08-10 20:07:42 +0100

    Betty 0-av file

--- 0-av.c
+++ 0-av.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int main(int ac, char **av) {
-         (void)ac;
-    int i = 1; // Start from av[1] to skip the program name in av[0]
+/**
+ * main - Entry point
+ * @ac: number of items
+ * @av: NULL terminated arr of str
+ *
+ * Return: arguments
+ */
 
-    while (av[i] != NULL) {
-        printf("%s\n", av[i]);
-        i++;
-    }
+int main(int ac, char **av)
+{
+	(void)ac;
+	int i = 1; /*Start from av[1] to skip the program name in av[0]*/
 
-    return 0;
+	while (av[i] != NULL)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+
+	return (0);
 }
 
---
+++ getlistex.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - main entry point
+ * @c: argument count
+ * @av: argument vector
+ *
+ * Return Always  0
+ */
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	FILE *filestream
+
+}
