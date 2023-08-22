57f82ff2f28dd22e24cf88be2bc5b728b335119c
kennyChukwuebuka <kenkinado@gmail>
2023-08-22 22:25:14 +0100

    Updated the function that reads input from user

--- 0-signalfunc.c
+++ 0-signalfunc.c
@@ -22,31 +22,31 @@ ssize_t custinp_buf(info_t *info, char **buff, size_t *c)
 	ssize_t rd = 0;
 	size_t len = 0;
 
-	if (!*c)
+	if (!*c) /*check if input buffer is empty*/
 	{
-		free(*buff);
+		free(*buff);/*free existing buffer*/
 		*buff = NULL;
-		signal(SIGINT, custsignal); /*undefined SIGINT*/
-#if USE_GETLINE
+		signal(SIGINT, custsignal); /*set custom signal handler (CTRL+C)*/
+#if USE_GETLINE/*Depending on USE_GETLINE macro, read input using getline*/
 		rd = getline(buff, &len, stdin);
 #else
 		rd = cust_getline(info, buff, &len);
 #endif
-		if (rd > 0)
+		if (rd > 0)/*if input is successful read*/
 		{
 			if ((*buff)[rd - 1] == '\n')
 			{
 				(*buff)[rd - 1] = '\0';
 				rd--;
 			}
-			info->linecount_flag = 1;
+			info->linecount_flag = 1;/*set flag indicating line is read*/
 			rp_com(*buff);
-			custbhist(info, *buff, info->histcount++);
+			custbhist(info, *buff, info->histcount++);/*store input in buffer*/
 			{
 				*c = rd;
 				info->cmd_buf = buff;
 			}
 		}
 	}
-	return (rd);
+	return (rd);/*return number of char read*/
 }
