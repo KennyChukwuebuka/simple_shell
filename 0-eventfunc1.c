c24735c7d6bb2266778f0d16b5e68911a43ed65d
kennyChukwuebuka <kenkinado@gmail>
2023-08-23 20:24:09 +0100

    Fixed the error of the history count

--- 0-eventfunc1.c
+++ 0-eventfunc1.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * custreadhst - Function that read file history
+ * custreadhst - Function that read and process the content
+ * of a history file
  * @info: Params
  * Return: 0
  */
@@ -11,39 +12,39 @@ int custreadhst(info_t *info)
 	struct stat fst;
 	char *buff = NULL, *f_name = cust_histfile(info);
 
-	if (!f_name)
+	if (!f_name)/*check is history file name is unavailable*/
 		return (0);
-	fdesc = open(f_name, O_RDONLY);
-	free(f_name);
-	if (fdesc == -1)
+	fdesc = open(f_name, O_RDONLY);/*open hist file for reading*/
+	free(f_name);/*free mem alloc for hist filename*/
+	if (fdesc == -1)/*check is opening the file failed*/
 		return (0);
-	if (!fstat(fdesc, &fst))
+	if (!fstat(fdesc, &fst))/*retrieve file status infomation*/
 		f_size = fst.st_size;
-	if (f_size < 2)
+	if (f_size < 2)/*if file size less that 2 byte ret 0*/
 		return (0);
-	buff = malloc(sizeof(char) * (f_size + 1));
-	if (!buff)
+	buff = malloc(sizeof(char) * (f_size + 1));/*alloc mem to read file cont*/
+	if (!buff)/*check if mem alloc failed*/
 		return (0);
-	len = read(fdesc, buff, f_size);
+	len = read(fdesc, buff, f_size);/*read content of file into buffer*/
 	buff[f_size] = 0;
-	if (len <= 0)
+	if (len <= 0)/*if read opr fail or ret no byte ret 0 after free*/
 		return (free(buff), 0);
-	close(fdesc);
-	for (i = 0; i < f_size; i++)
+	close(fdesc);/*close the history file*/
+	for (i = 0; i < f_size; i++)/*process each chr in buf and split content*/
 		if (buff[i] == '\n')
 		{
 			buff[i] = 0;
 			custbhist(info, buff + lasthst, line++);
 			lasthst = i + 1;
 		}
-	if (lasthst != i)
+	if (lasthst != i)/*if there are reminaing char after loop process*/
 		custbhist(info, buff + lasthst, line++);
-			free(buff);
-			info->histcount = line;
-			while (info->histcount-- >= HIST_MAX)
+			free(buff);/*free the buff*/
+			info->histcount = line;/*update the history count in the info sruct*/
+			while (info->histcount-- >= HIST_MAX)/*trim hist list to not exced max*/
 				del_node_i(&(info->history), 0);
-			custre_numhst(info);
-			return (info->histcount);
+			custre_numhst(info);/*re number history entries*/
+			return (info->histcount);/*return updated history count*/
 }
 /**
  * custbhist - Function to add entry to list
@@ -56,16 +57,17 @@ int custbhist(info_t *info, char *buff, int c)
 {
 	list_t *node = NULL;
 
-	if (info->history)
-		node = info->history;
-	addnodeto_end(&node, buff, c);
+	if (info->history)/*check if history list is not empty*/
+		node = info->history;/*if not empty assign to node*/
+	addnodeto_end(&node, buff, c);/*add a new node with the buff and c*/
 
-	if (!info->history)
+	if (!info->history)/*if list was empty update info structure*/
 		info->history = node;
-	return (0);
+	return (0);/*success ret 0*/
 }
 /**
- * custre_numhst - Function that re_number list
+ * custre_numhst - Function that re_number history entries
+ * in a linked list
  * @info: Params
  * Return: hist
  */
@@ -74,10 +76,12 @@ int custre_numhst(info_t *info)
 	list_t *node = info->history;
 	int i = 0;
 
-	while (node)
+	while (node)/*iterate through hist list*/
 	{
-		node->num = i++;
-		node = node->next;
+		node->num = i++;/*assign val i to the num member of history list*/
+		node = node->next;/*move to next node in the l-list*/
 	}
+	/*update histcount member in the info struct with final value */
+	/*return the updated value*/
 	return (info->histcount = i);
 }
